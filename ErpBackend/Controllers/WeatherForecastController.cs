d5ac05182271b7a5a195fac2d966ef0eb14b41d4
Piorosen <aoikazto@gmail.com>
2021-11-06 13:19:09 +0900

    asp https 적용.

--- ErpBackend/Controllers/WeatherForecastController.cs
+++ ErpBackend/Controllers/WeatherForecastController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
 
 namespace ErpBackend.Controllers
 {
     [ApiController]
-    //[Route("[controller]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,15 +20,17 @@ namespace ErpBackend.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet("/")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            Request.T
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- ErpBackend/Startup.cs
+++ ErpBackend/Startup.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +28,7 @@ namespace ErpBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
         }
 
@@ -35,12 +40,12 @@ namespace ErpBackend
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-               
                 endpoints.MapControllers();
             });
         }
