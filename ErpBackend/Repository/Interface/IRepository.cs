bb4538aa606770b75c5f6adc2095b0d1ce766d59
Piorosen <aoikazto@gmail.com>
2021-11-12 12:31:03 +0900

    데이터베이스와 비즈니스 코드와 분리하기 위해서 레포지토리 패턴 적용

---
+++ ErpBackend/Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ErpBackend.Repository.Interface
+{
+    public interface IRepository<T, ID>
+    {
+        T Update(T value);
+        T Add(T data);
+
+        bool Delete(T data);
+        bool Delete(ID id);
+
+        IQueryable<T> Get();
+    }
+}
---
+++ ErpBackend/Repository/RaspberryRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ErpBackend.Models;
+using ErpBackend.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpBackend.Repository
+{
+    public class RaspberryRepository : IRepository<RaspberryModel, int>, IDisposable
+    {
+        private ErpContext context;
+        private bool disposed = false;
+
+
+        public RaspberryRepository(ErpContext context)
+        {
+            this.context = context;
+        }
+
+        public RaspberryModel Add(RaspberryModel data)
+        {
+            var result = context.Raspberry.Add(data);
+            result.Reload();
+            return result.Entity;
+        }
+
+        public bool Delete(RaspberryModel data)
+        {
+            var result = context.Raspberry.Remove(data);
+            return result.State == EntityState.Deleted;
+        }
+        public bool Delete(int id)
+        {
+            return Delete(context.Raspberry.Find(id));
+        }
+
+        public IQueryable<RaspberryModel> Get()
+        {
+            return context.Raspberry;
+        }
+
+        public RaspberryModel Update(RaspberryModel value)
+        {
+            context.Raspberry.Attach(value);
+            context.Entry(value).State = EntityState.Modified;
+
+            return value;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.SaveChanges();
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
