9e52a4a77ca637453c1b38832c379fb54edab330
Piorosen <aoikazto@gmail.com>
2021-11-16 23:53:35 +0900

    아두이노 통신 모듈 추가

--- ErpBackend/Controllers/devices/ArduinoController.cs
+++ ErpBackend/Controllers/devices/ArduinoController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using ErpBackend.Models;
+using ErpBackend.Repository;
+using ErpBackend.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -11,24 +14,44 @@ using Microsoft.Extensions.Logging;
 
 namespace ErpBackend.Controllers.Devices
 {
-    //[ApiController]
-    //public class ArdunioController : ControllerBase
-    //{
-    //    private readonly TodoContext _context;
-    //    private readonly ILogger<ArdunioController> _logger;
+    [ApiController]
+    [Route("/devices/arduino")]
+    public class ArdunioController : Controller
+    {
+        private readonly IRepository<ArduinoModel, int> repository;
+        private readonly ILogger<ArdunioController> _logger;
 
-    //    // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
 
-    //    public ArdunioController(ILogger<ArdunioController> logger, TodoContext context)
-    //    {
-    //        _context = context;
-    //        _logger = logger;
-    //    }
+        public ArdunioController(ILogger<ArdunioController> logger, ErpContext context)
+        {
+            //this.repository = new ArduinoRepository(context);
+            this._logger = logger;
 
-    //    [HttpGet("/devices/arduino")]
-    //    public IEnumerable<Todsso> Get()
-    //    {
-    //        return _context.Todo.ToArray();
-    //    }
-    //}
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Json(new { result = true });
+        }
+
+        [HttpGet("{id}/status")]
+        public IActionResult GetStatus(string id)
+        {
+            return Json(new { result = true });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            return Json(new { result = true });
+        }
+
+        [HttpPost("{id}/status")]
+        public IActionResult PostStatus([FromBody] JsonDocument json, string id)
+        {
+            return Json(new { result = true });
+        }
+    }
 }
